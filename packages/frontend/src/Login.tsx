8f851185d18f1255a1ea3b3803485728a1fdb4a4
xandykati98 <47799117+xandykati98@users.noreply.github.com>
2025-02-22 21:30:59 -0300

    responsive fix

--- packages/frontend/src/Login.tsx
+++ packages/frontend/src/Login.tsx
@@ -90,7 +90,7 @@ const Login: React.FC = () => {
         </div>
       </div>
       {/* Right Panel */}
-      <div className="lg:p-8">
+      <div className="right-panel">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">Entre na sua conta</h1>
